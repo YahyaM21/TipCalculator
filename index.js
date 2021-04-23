6954dd4ba6ef78cc65cffbdf3f647d295fcc7010
Yahya Mohamed <46172696+YahyaM21@users.noreply.github.com>
2021-04-23 13:45:07 -0500

    Percent over 100
    added an if statement so that if the user inputs a tip amount greater than 100% code will display a message

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 function calcTip(){
+    var ignore = ["$", "%"];
     var bill = Number(document.getElementById("bill").value);
     var tip = Number(document.getElementById("percentage").value);
-
-    var calculation = Math.floor(bill * (tip/100));
     
-    document.getElementById("tip").innerHTML = "You should tip " + "$" + calculation;
+    if (tip > 100){
+      document.getElementById("tip").innerHTML = "You can't tip over 100%";
+    }else {
+      var calculation = Math.floor(bill * (tip/100));
+      document.getElementById("tip").innerHTML = "You should tip " + "$" + calculation;
+    }
+    
 
 }
 document.getElementById("calc").onclick = function(){
     calcTip();
 }
-
